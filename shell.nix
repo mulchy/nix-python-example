a17dd3f43de5dc2f479c734560961a33c378be65
Ryan Mulcahy <ryan.mulcahy@blackwellsecurity.com>
2024-11-13 12:43:24 -0600

    add node/npm/tsc

--- shell.nix
+++ shell.nix
@@ -17,15 +17,34 @@ in
     pkgs.xh
   ];
 
-  languages.python = {
-    enable = true;
-    venv.enable = true;
-    uv = {
+  languages = {
+    python = {
       enable = true;
-      package = pkgs-unstable.uv;
-      sync.enable = true;
+      venv.enable = true;
+      uv = {
+        enable = true;
+        package = pkgs-unstable.uv;
+        sync.enable = true;
+      };
+    };
+
+    javascript = {
+      enable = true;
+      # 22 not available in cachix/devenv-nixpkgs/rolling yet
+      # This builds from source on my computer and takes a while
+      # maybe go with 21 for now
+      package = pkgs-unstable.nodejs_22;
+      npm = {
+        enable = true;
+        install.enable = true;
+      };
+    };
+
+    typescript = {
+      enable = true;
     };
   };
+
 
   enterTest = ''
     echo "Running tests"
@@ -35,6 +54,9 @@ in
     xh --version | grep "0.21.0"
     uv --version | grep "0.4.30"
     python3 --version | grep "3.12.7"
+    node --version | grep "22.10.0"
+    npm --version | grep "10.9.0"
+    tsc --version | grep "5.4.4"
   '';
 
   pre-commit.hooks.ruff.enable = true;
